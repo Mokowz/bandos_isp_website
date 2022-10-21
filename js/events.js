04d56ae688b186299ebb015bb06f31beedbc3b2b
Mokowz <ronniemokaya30@gmail.com>
2022-10-22 00:25:50 +0300

    Added icons and responsiveness

---
+++ js/events.js
@@ -0,0 +1,18 @@
+const ham_menu = document.getElementById("ham_menu")
+const nav_menu = document.getElementById("navmenu")
+const call = document.getElementById("call")
+
+
+ham_menu.addEventListener("click", () => {
+    nav_menu.classList.toggle("show")
+})
+
+// Cards
+const card = document.getElementsByClassName("serv_card")
+
+for (let i=0; i < card.length; i++){
+    
+    card[i].addEventListener("click", () => {
+        card[i].classList.toggle("serv_act")
+    })
+}
